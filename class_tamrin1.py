cebb08a191965d1f20ac995d0482b34fcc4840da
arman82nik <mohsennik1352@gmail.com>
2025-08-03 14:50:23 +0330

    Add files via upload

---
+++ class_tamrin1.py
@@ -0,0 +1,65 @@
+from tkinter import *
+
+from validation import name_checker
+
+phone_list=[]
+list_total=[]
+
+def save_click():
+    phone = {"name":name.get(),"quntity":quantity.get(),"price":price.get(),}
+    if name_checker(name.get()):
+        total_zarb = {quantity.get()*price.get()}
+        list_total.append(total_zarb)
+        phone_list.append(phone)
+        print(phone_list)
+        print(list_total)
+        name.set("")
+        quantity.set(0)
+        price.set(0)
+        total.set(total_zarb)
+
+
+
+
+
+
+
+
+
+window = Tk()
+window.title("phone")
+window.configure(background="gold")
+window.geometry("400x400")
+
+Label(window, text="name").place(x=20, y=20)
+name =StringVar()
+Entry(window, textvariable=name).place(x=80, y=20)
+
+Label(window, text="quantity").place(x=20, y=60)
+quantity = IntVar()
+quantity.set(0)
+Entry(window, textvariable=quantity).place(x=80, y=60)
+
+Label(window, text="price").place(x=20, y=100)
+price = IntVar()
+price.set(0)
+Entry(window, textvariable=price).place(x=80, y=100)
+
+Label(window, text="total").place(x=80, y=150)
+total =IntVar()
+total.set(0)
+Entry(window, textvariable=total).place(x=130, y=150)
+
+
+
+Button(window,text="save",width = 10,command=save_click).place(x=80,y=180)
+
+
+
+
+
+
+
+
+
+window.mainloop()
